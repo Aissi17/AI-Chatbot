7c337b6fb8c98013bf5f8e8e1a308f0890bd7292
Islam <islam@intrafentech.com>
2025-02-13 22:51:21 +0100

    [DEV] integrate google gemini

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import { useState } from "react";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 import styles from "./App.module.css";
 import { Chat } from "./components/Chat/Chat";
 import { Controls } from "./components/Controls/Controls";
+
+const googleai = new GoogleGenerativeAI(import.meta.env.VITE_GOOGLE_AI_API_KEY);
+const model = googleai.getGenerativeModel({ model: "gemini-2.0-flash" });
+const chat = model.startChat({ history: [] });
+
 function App() {
   const [messages, setMessages] = useState([]);
 
-  function handleContentSend(content) {
-    setMessages((prevMessages) => [...prevMessages, { content, role: "user" }]);
+  function addMessage(message) {
+    setMessages((prevMessages) => [...prevMessages, message]);
+  }
+
+  async function handleContentSend(content) {
+    addMessage({ content, role: "user" });
+    try {
+      const result = await chat.sendMessage(content);
+      addMessage({ content: result.response.text(), role: "assistant" });
+    } catch (error) {
+      addMessage({ content: "Busy servers.", role: "system" });
+    }
   }
 
   return (
