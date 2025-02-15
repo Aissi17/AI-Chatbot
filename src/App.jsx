0760bca6b1029d62be7098d892e4f6215e4f1091
Islam <islam@intrafentech.com>
2025-02-15 14:43:02 +0100

    [DEV] add loader while texting the bot

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { Assistant } from "./assistants/google-ai";
 import styles from "./App.module.css";
 import { Chat } from "./components/Chat/Chat";
 import { Controls } from "./components/Controls/Controls";
+import { Loader } from "./components/Loader/Loader";
 
 function App() {
   const googleAssistant = new Assistant();
   const [messages, setMessages] = useState([]);
+  const [isLoading, setLoading] = useState(false);
 
   function addMessage(message) {
     setMessages((prevMessages) => [...prevMessages, message]);
@@ -15,17 +17,21 @@ function App() {
 
   async function handleContentSend(content) {
     addMessage({ content, role: "user" });
+    setLoading(true);
     try {
       const result = await googleAssistant.chat(content);
       // const result = await googleAssistant.chat(content, messages);
       addMessage({ content: result, role: "assistant" });
     } catch (error) {
       addMessage({ content: "Busy servers.", role: "system" });
+    } finally {
+      setLoading(false);
     }
   }
 
   return (
     <div className={styles.App}>
+      {isLoading && <Loader />}
       <header className={styles.Header}>
         <img className={styles.Logo} src="/bot.png" />
         <h2 className={styles.Title}>AI CHATBOT</h2>
